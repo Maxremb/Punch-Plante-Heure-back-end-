642e6de3e3b5ab5d82e843a408c4ee5291982fa5
Lea369 <coston.lea@gmail.com>
2020-04-01 12:56:20 +0200

    diagramme update

---
+++ Module_Persistence/src/main/java/com/fr/adaming/repositories/IPlanteModelRepository.java
@@ -0,0 +1,10 @@
+package com.fr.adaming.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fr.adaming.entity.PlanteModel;
+
+public interface IPlanteModelRepository extends JpaRepository<PlanteModel, Integer> {
+
+	
+}
