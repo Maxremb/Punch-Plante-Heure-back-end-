ac0f6789c984e9ea7b0ac96c43f699b9d3f4ec60
ClaraCdt <clara.cadet@yahoo.fr>
2020-04-01 12:26:09 +0200

    javadoc jardin et test jardin repository

--- Module_Persistence/src/main/java/com/fr/adaming/entity/Sol.java
+++ Module_Persistence/src/main/java/com/fr/adaming/entity/Sol.java
@@ -1,5 +1,10 @@
 package com.fr.adaming.entity;
 
+/**
+ * Classe d'énumération des différents types de sol
+ * @author clara
+ * @since 1.0.x
+ */
 public enum Sol {
 	Sableux,
 	Argileux,
--- Module_Persistence/src/main/java/com/fr/adaming/repositories/IJardinRepository.java
+++ Module_Persistence/src/main/java/com/fr/adaming/repositories/IJardinRepository.java
@@ -9,16 +9,21 @@ import org.springframework.stereotype.Repository;
 
 import com.fr.adaming.entity.User;
 
+/**
+ * Interface repository responsable de la communication avec la base de données pour l'entité Jardin 
+ * @author clara
+ * @since 1.0.x
+ */
 @Repository
 public interface IJardinRepository extends JpaRepository<Jardin, Long>{
 
 	public List<Jardin> findByNom(String nom);
 	
-	@Query(value = "from Jardin where id= :idUtilisateur")
+	@Query(value = "from Jardin where utilisateur_id= :idUtilisateur")
 	public List<Jardin> findByUtilisateur(@Param(value = "idUtilisateur") Integer idUtilisateur);
 	
-	@Query(value = "from Jardin where id= :idUtilisateur")
-	public List<Jardin> findByDepartement(@Param(value = "idUtilisateur") Integer idUtilisateur);
+	@Query(value = "from Jardin where departement_id= :idDepartement")
+	public List<Jardin> findByDepartement(@Param(value = "idDepartement") Integer idDepartement);
 	
 	
 }
