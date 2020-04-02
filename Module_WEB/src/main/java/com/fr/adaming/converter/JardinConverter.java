f1ee282c9cce8e8dda48202fc66a5d68034645d6
ClaraCdt <clara.cadet@yahoo.fr>
2020-04-02 11:35:31 +0200

    Jardin converter part 1

--- Module_WEB/src/main/java/com/fr/adaming/converter/JardinConverter.java
+++ Module_WEB/src/main/java/com/fr/adaming/converter/JardinConverter.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fr.adaming.dto.DepartementDto;
 import com.fr.adaming.dto.JardinCreateDto;
 import com.fr.adaming.dto.JardinUpdateDto;
 import com.fr.adaming.dto.UtilisateurCreateDto;
 import com.fr.adaming.dto.UtilisateurUpdateDto;
+import com.fr.adaming.entity.Departement;
 import com.fr.adaming.entity.Jardin;
 import com.fr.adaming.entity.Utilisateur;
 
 public class JardinConverter implements IConverter<JardinCreateDto, JardinUpdateDto, Jardin>{
 	
 	@Autowired
-	private IConverterDepartement convertDep;
+	private IConverterDepartement<Departement, DepartementDto> convertDep;
 	
 	@Autowired
 	private IConverter<UtilisateurCreateDto, UtilisateurUpdateDto, Utilisateur> convertUtil;
@@ -22,7 +24,7 @@ public class JardinConverter implements IConverter<JardinCreateDto, JardinUpdate
 	@Override
 	public Jardin convertCreateDtoToEntity(JardinCreateDto createDto) {
 		Jardin entite = new Jardin();
-		entite.setDepartement(convertDep.(createDto.getDept()));
+		entite.setDepartement(convertDep.convertDtoToEntity((createDto.getDept())));
 		entite.setLargeur(createDto.getWidth());
 		entite.setLongueur(createDto.getLength());
 		entite.setNom(createDto.getName());
@@ -34,14 +36,22 @@ public class JardinConverter implements IConverter<JardinCreateDto, JardinUpdate
 
 	@Override
 	public JardinCreateDto convertEntityToCreateDto(Jardin entity) {
-		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
 
 	@Override
 	public Jardin convertUpdateDtoToEntity(JardinUpdateDto updateDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Jardin entite = new Jardin();
+		entite.setDepartement(convertDep.convertDtoToEntity((updateDto.getDept())));
+		entite.setLargeur(updateDto.getWidth());
+		entite.setLongueur(updateDto.getLength());
+		entite.setNom(updateDto.getName());
+		entite.setSol(updateDto.getGround());
+		entite.setUtilisateur(convertUtil.convertCreateDtoToEntity(updateDto.getUser()));
+		entite.setId(updateDto.getIdentifier());
+		return entite;
 	}
 
 	@Override
