d29ff7b1c31de12ed33a0413f413973d2920a71b
Lea369 <coston.lea@gmail.com>
2020-04-02 11:48:24 +0200

    creation periodeConverter

---
+++ Module_WEB/src/main/java/com/fr/adaming/converter/PeriodeConverter.java
@@ -0,0 +1,60 @@
+package com.fr.adaming.converter;
+
+import java.util.List;
+
+import com.fr.adaming.dto.PeriodeCreateDto;
+import com.fr.adaming.dto.PeriodeUpdateDto;
+import com.fr.adaming.entity.Periode;
+
+public class PeriodeConverter implements IConverter<PeriodeCreateDto, PeriodeUpdateDto, Periode> {
+
+	@Override
+	public Periode convertCreateDtoToEntity(PeriodeCreateDto createDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PeriodeCreateDto convertEntityToCreateDto(Periode entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Periode convertUpdateDtoToEntity(PeriodeUpdateDto updateDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PeriodeUpdateDto convertEntityToUpdateDto(Periode entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Periode> convertListCreateDtoToEntity(List<PeriodeCreateDto> listeCreateDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PeriodeCreateDto> convertListEntityToCreateDto(List<Periode> listeEntity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Periode> convertListUpdateDtoToEntity(List<PeriodeUpdateDto> listeUpdateDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PeriodeUpdateDto> convertListEntityToUpdateDto(List<Periode> listeEntity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
