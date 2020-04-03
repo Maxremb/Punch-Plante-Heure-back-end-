b2535f38579c1a6edcb34a8ad8e97bfd46df2d8a
gregoire <gregoire_brebner@hotmail.com>
2020-04-03 15:18:04 +0200

    Jardin repo sql fix

--- Module_Persistence/src/main/java/com/fr/adaming/repositories/IJardinRepository.java
+++ Module_Persistence/src/main/java/com/fr/adaming/repositories/IJardinRepository.java
@@ -31,7 +31,8 @@ public interface IJardinRepository extends JpaRepository<Jardin, Integer>{
 	 * @param idUtilisateur
 	 * @return une liste de jardin possédés par l'utilisateur ayant cet identifiant
 	 */
-	@Query(value = "from Jardin where utilisateur_id= :idUtilisateur")
+	//@Query(value = "from Jardin where utilisateur_id= :idUtilisateur")
+	@Query(value = "select * from jardin where utilisateur_id= :idUtilisateur", nativeQuery = true)
 	public Page<Jardin> trouveParUtilisateur(Pageable pageable, @Param(value = "idUtilisateur") Integer idUtilisateur);
 	
 	/**
@@ -39,7 +40,7 @@ public interface IJardinRepository extends JpaRepository<Jardin, Integer>{
 	 * @param idDepartement correspondant au numéro unique de département
 	 * @return une liste de jardin localisé dans le département ayant ce numéro
 	 */
-	@Query(value = "from Jardin where departement_id= :idDepartement")
+	@Query(value = "select * from jardin where departement_id= :idDepartement", nativeQuery = true)
 	public Page<Jardin> trouveParDepartement(Pageable pageable, @Param(value = "idDepartement") Integer idDepartement);
 	
 	
