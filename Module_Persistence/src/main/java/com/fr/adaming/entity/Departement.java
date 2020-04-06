886190410708af16dbcffe469429455cfebf1785
jmmathevet <jmmathevet@gmail.com>
2020-04-06 20:37:50 +0200

    modification

--- Module_Persistence/src/main/java/com/fr/adaming/entity/Departement.java
+++ Module_Persistence/src/main/java/com/fr/adaming/entity/Departement.java
@@ -1,14 +1,8 @@
 package com.fr.adaming.entity;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
