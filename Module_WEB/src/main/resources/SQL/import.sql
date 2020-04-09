32a45b685c2932e449a5e4bd135eb1cc4a172b7e
gregoire <gregoire_brebner@hotmail.com>
2020-04-09 10:11:55 +0200

    fichier source import

---
+++ Module_WEB/src/main/resources/SQL/import.sql
@@ -0,0 +1,3 @@
+source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/departement.sql;
+source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/plante_model_names.sql;
+source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/retention.sql;
