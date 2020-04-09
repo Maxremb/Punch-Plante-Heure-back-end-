9f9eb0b1cd098c4c85501af0105fa18d1074e0d6
IsalineMllt <isalinemillet38@gmail.com>
2020-04-09 12:42:30 +0200

    database SQL utilisable

--- Module_WEB/src/main/resources/SQL/import.sql
+++
@@ -1,3 +0,0 @@
-source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/departement.sql;
-source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/plante_model_names.sql;
-source  C:/Users/IN-LY-020/Desktop/INTI/Projet Final/SQL/retention.sql;
