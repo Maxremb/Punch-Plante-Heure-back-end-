4277ddcf429c06716b485948e7dd26ffd4d57558
unknown <maxime.rembert@gmail.com>
2020-04-09 17:49:49 +0200

    test utilisateur repo OK

--- Module_Persistence/src/test/java/com/fr/adaming/repositories/IUtilisateurRepositoryTest.java
+++ Module_Persistence/src/test/java/com/fr/adaming/repositories/IUtilisateurRepositoryTest.java
@@ -28,35 +28,53 @@ public class IUtilisateurRepositoryTest {
 	private static final String pseudo = "extra terrestre";
 	// ID EMAIL MDP PSEUDO NOM PRENOM NON NULLABLE
 
-	// ***************************************************************
-	// FIND BY NOM AND PRENOM
+
+	/**
+	 * Test de READ BY NOM AND PRENOM avec valeurs correcte
+	 * retourne l'entite
+	 */
 	@Test
-	@Sql(statements = "INSERT INTO utilisateur (nom,prenom,email,mdp,pseudo) VALUES ('jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "INSERT INTO utilisateur (id,nom,prenom,email,mdp,pseudonyme) VALUES (1,'jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql (statements = "DELETE FROM utilisateur", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	public void TestReadByNomAndPrenomWithValidParam_ShouldreturnEntite() {
 		Utilisateur user = repo.findByNomAndPrenom(nom, prenom);
 
-		assertThat(user);
+		assertThat(user).isNotNull().hasFieldOrPropertyWithValue("email", "kiki@trail.fr");
 	}
 
+	/**
+	 * Test de READ BY NOM AND PRENOM avec tout les param null
+	 * retourne null
+	 */
 	@Test
-	@Sql(statements = "INSERT INTO utilisateur (nom,prenom,email,mdp,pseudo) VALUES ('jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "INSERT INTO utilisateur (id,nom,prenom,email,mdp,pseudonyme) VALUES (1,'jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql (statements = "DELETE FROM utilisateur", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	public void TestReadByEmailAndMdpWithNullParam_ShouldReturnNull() {
-		assertThat((repo.findByNomAndPrenom(null, null)));
+		assertThat((repo.findByNomAndPrenom(null, null))).isNull();
 	}
 
+	/**
+	 * Test de READ BY NOM AND PRENOM avec un param null
+	 * retourne 
+	 */
 	@Test
-	@Sql(statements = "INSERT INTO utilisateur (nom,prenom,email,mdp,pseudo) VALUES ('jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "INSERT INTO utilisateur (id,nom,prenom,email,mdp,pseudonyme) VALUES (1,'jornet','kilian','kiki@trail.fr','4TEST','extra terrestre') ", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql (statements = "DELETE FROM utilisateur", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	public void TestReadByEmailAndMdpWithOneNullParam_ShouldReturnNull() {
 		Utilisateur user = repo.findByNomAndPrenom(nom, null);
 
-		assertThat(user);
+		assertThat(user).isNull();
 	}
 
+	/**
+	 * Test de READ BY NOM AND PRENOM avec DB vide
+	 * retourne null
+	 */
 	@Test
 	public void TestReadByEmailAndMdpNoDB_shouldReturnNull() {
 		Utilisateur user = repo.findByNomAndPrenom(nom, prenom);
 
-		assertThat(user);
+		assertThat(user).isNull();
 	}
 
 }
